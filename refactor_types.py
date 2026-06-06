31adc97032402bee719f5c254c63433a7d885c4a
sou0 <pekwd1228@gmail.com>
2026-06-06 17:35:35 -0300

    chore: remove python refactor scripts

--- refactor_types.py
+++
@@ -1,7 +0,0 @@
-import re
-
-file_path = 'plugin classificação.php'
-with open(file_path, 'r', encoding='utf-8') as f:
-    content = f.read()
-
-# I will write the python script to parse and replace the chunks
